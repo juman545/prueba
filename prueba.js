81d68786f087b226007abc16381c926542fbee39
Juan Manuel Garzón <130332242+juman545@users.noreply.github.com>
2023-04-24 18:00:08 -0500

    nuevos documentos
    avance pero no tando

---
+++ prueba.js
@@ -0,0 +1,14 @@
+let cardio = ["corazón de AMOR", "sistole", "diastole"]
+let comer = ["100kg diarios", "50kg diarios", "25kg diarios"]
+console.log("hola mundo:", cardio.toLowerCase())
+console.log("CAMBIOS:", cardio +" "+ 2 )
+console.log(cardio, comer)
+
+function forEachprueba(value, index) {
+    console.log(cardio)
+    console.log(comer)
+    console.log(cardio + comer)
+
+cardio.forEach(forEachprueba)
+}
+console.log("cardio")
