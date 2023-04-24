11b13dcb4fe20e4780c5825a86467e8be1835a8b
juman545 <130332242+juman545@users.noreply.github.com>
2023-04-24 18:11:44 -0500

    editar js, mejorar codigo

--- prueba.js
+++ prueba.js
@@ -1,6 +1,5 @@
 let cardio = ["corazón de AMOR", "sistole", "diastole"]
 let comer = ["100kg diarios", "50kg diarios", "25kg diarios"]
-console.log("hola mundo:", cardio.toLowerCase())
 console.log("CAMBIOS:", cardio +" "+ 2 )
 console.log(cardio, comer)
 
